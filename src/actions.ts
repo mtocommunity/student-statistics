82a7d83889876ec0d28c2ad79067bd0492c1178a
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2025-08-18 15:09:04 -0500

    refactor: Rename files

--- src/actions.ts
+++ src/actions.ts
@@ -1,7 +1,7 @@
 import { authAction } from "@/auth/action/auth-actions"
 import { courseAction } from "@/course/action/course-actions"
-import { examAction } from "@/exam/action/exam-action"
-import { semesterAction } from "@/semester/action/semester-action"
+import { examAction } from "@/exam/action/exam-actions"
+import { semesterAction } from "@/semester/action/semester-actions"
 
 export const server = {
   auth: authAction,
